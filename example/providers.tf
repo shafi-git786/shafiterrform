a426576fd21fb9b418b37fb3b060245029833852
shafi-git786 <85149468+shafi-git786@users.noreply.github.com>
2023-06-22 20:31:43 +0530

    Update providers.tf

--- example/providers.tf
+++ example/providers.tf
@@ -18,7 +18,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "tamopstfstates"
     storage_account_name = "tamopstfsha"
-    container_name       = "tfstatedevops"
+    container_name       = "terraformgithubexample"
     key                  = "terraformgithubexample.tfstate"
   }
 }
