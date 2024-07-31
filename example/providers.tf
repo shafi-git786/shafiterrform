af8d0189650a6e53196282b8d2d14a0544a6b4e5
shafi-git786 <85149468+shafi-git786@users.noreply.github.com>
2024-08-01 00:59:31 +0530

    Update providers.tf

--- example/providers.tf
+++ example/providers.tf
@@ -16,11 +16,11 @@ data "azurerm_client_config" "current" {}
 
 terraform {
   backend "azurerm" {
-    resource_group_name  = "shafi-rg"
-    storage_account_name = "shafistg12"
-    container_name       = "shafistg12cont"
+    resource_group_name  = "terra-rg"
+    storage_account_name = "teststgacnt6789backend"
+    container_name       = "container-bknd"
     key                  = "terraformgithubexample.tfstate"
-    subscription_id       = "d4281e96-cd35-4be0-b807-b446ba406ea8"
-    tenant_id             = "ae229306-f857-4091-a542-342fbe575384"
+    subscription_id       = "739b9cfd-8e02-47a9-8e2b-f5c8c7e1d547"
+    tenant_id             = "4a00ec52-96a9-4a3d-8701-f5ce094b0317"
   }
 }
