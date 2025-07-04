7707ed8b8c01bddc26b47df198cf228f5a724ce7
shafi-git786 <85149468+shafi-git786@users.noreply.github.com>
2025-07-04 08:24:56 +0530

    added push trigger and commented storage account

--- example/main.tf
+++ example/main.tf
@@ -3,17 +3,17 @@ resource "azurerm_resource_group" "sharg" {
   location = "WEST US"
 }
 
-resource "azurerm_storage_account" "shastg1" {
-  name                     = "storagenewshaikssss15"
-  resource_group_name      = azurerm_resource_group.sharg.name
-  location                 = azurerm_resource_group.sharg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# resource "azurerm_storage_account" "shastg1" {
+#   name                     = "storagenewshaikssss15"
+#   resource_group_name      = azurerm_resource_group.sharg.name
+#   location                 = azurerm_resource_group.sharg.location
+#   account_tier             = "Standard"
+#   account_replication_type = "LRS"
 
-  tags = {
-    environment = "staging"
-  }
-}
+#   tags = {
+#     environment = "staging"
+#   }
+# }
 
 resource "random_pet" "azurerm_mssql_server_name" {
   prefix = "sql"
